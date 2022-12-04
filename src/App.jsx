4e95eab5e8bf0fe718485120af26a6e62a4c12b8
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-04 17:20:39 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+
+import Navbar from "./assets/components/Navbar";
+import Hero from "./assets/components/Hero";
+import Stats from "./assets/components/Stats";
+import Business from "./assets/components/Business";
+import Biling from "./assets/components/Biling";
+import CardDeal from "./assets/components/CardDeal";
+import Testimonials from "./assets/components/Testimonials";
+import Clients from "./assets/components/Clients";
+import CTA from "./assets/components/CTA";
+import Footer from "./assets/components/Footer";
+
 import styles from "./style";
 
 const App = () => (
@@ -6,26 +18,26 @@ const App = () => (
 
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Hero />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Biling
-        CardDeal
-        Testimonials
-        Clients
-        CTA
-        Footer
+        <Stats />
+        <Business />
+        <Biling />
+        <CardDeal  />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
 
