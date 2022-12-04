38c32eace09b73bf154fb970db9eb8f286bd9349
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-04 17:16:15 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,30 @@ import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    
+
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Hero</div>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Hero</div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Biling
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
     </div>
 
   </div>
---
+++ src/assets/components/Biling.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Biling = () => {
+  return (
+    <div>Biling</div>
+  )
+}
+
+export default Biling
