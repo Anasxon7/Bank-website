8c0597ed9eddfbba01c812862a4d77758c4a63d5
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-04 17:07:44 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div>
-      
+    
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>Navbar</div>
     </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>Hero</div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>Hero</div>
+    </div>
+
   </div>
 );
 
-export default App
+export default App;
