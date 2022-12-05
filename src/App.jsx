9349822ce577a5007e1a095ccfba0433c0c7e8c8
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-05 12:47:29 +0500

    navbar hero

--- src/App.jsx
+++ src/App.jsx
@@ -22,24 +22,24 @@ const App = () => (
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
-    </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Biling />
-        <CardDeal  />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Biling />
+          <CardDeal  />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
       </div>
-    </div>
 
   </div>
 );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,6 +26,17 @@ const Navbar = () => {
         onClick={() => setToggle((prev) => !prev)} />
        </div>
 
+       <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+        <ul className='list-none flex flex-col justify-end items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id}
+          className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+       </ul>
+       </div>
+
     </nav>
   )
 }
