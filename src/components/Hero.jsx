655c9dafb568ab556402bbbbc8c255dd62798f3c
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-05 12:54:37 +0500

    navbar hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,8 @@ const Hero = () => {
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px] " />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">20%</span>
-            Discount For {" "}
-            <span className="text-white">1 Month</span>
-            Account
+            <span className="text-white">20%</span> Discount For {" "}
+            <span className="text-white">1 Month</span> Account
           </p>
         </div>
       </div>
