fdd20f3e8069f8f3284269af208bd914009ae627
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-06 21:10:06 +0500

    Business

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { features } from '../constants'
+import styles, { latout } from '../style'
+import Button from './Button'
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business,
+         <br className='sm:block hidden' /> we'll handle the money.</h2>
+      </div>
+    </section>
   )
 }
 
