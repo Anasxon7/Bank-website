ef798ebf8957870a0f902bb94ab9a61a82a58ee0
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-08 14:38:40 +0500

    biling

--- src/components/Biling.jsx
+++ src/components/Biling.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { apple, bill, google} from '../assets'
+import styles, { layout } from '../style'
 
 const Biling = () => {
   return (
-    <div>Biling</div>
+    <section id='product' className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="biling" className='w-[100%] h-[100%] relative z-[5]' />
+        
+        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
+        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
+      </div>
+    </section>
   )
 }
 
