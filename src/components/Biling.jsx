a08d005ce660d94012173fb38b6de3101f4a257e
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-08 14:52:19 +0500

    biling

--- src/components/Biling.jsx
+++ src/components/Biling.jsx
@@ -10,6 +10,18 @@ const Biling = () => {
         <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
         <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
       </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden' /> biling & invoicing</h2>
+        <p className={` ${styles.paragraph} max-w=[470px] mt-5`}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. 
+          Fusce ipsum orci rhoncus aliporttitor inteder platea placerat.
+        </p>
+
+        <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+          <img src={apple} alt="apple_store" className='w-[128px] h-[42px] mr-5 cursor-pointer' />
+          <img src={google} alt="google_play" className='w-[128px] h-[42px] mr-5 cursor-pointer' />
+        </div>
+      </div>
     </section>
   )
 }
