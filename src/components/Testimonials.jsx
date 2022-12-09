770270d5bd1ff3d17798fdb900d4df6057493817
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-10 01:11:12 +0500

    FeedbackCard

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,3 +1,4 @@
+import { card } from '../assets'
 import { feedback } from '../constants'
 import styles from '../style'
 import FeedbackCard from './FeedbackCard'
@@ -12,9 +13,19 @@ const  Testimonials = () => (
          md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
             <h1 className={styles.heading2}>What people are <br className='sm:block hidden' />
             saying about us</h1>
-            <div className=''>
-
+            <div className='w-full md:mt-0 mt-6'>
+                <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+                    Everything you nrrd to accept card payments and grow 
+                    business anywhere on the planrt.
+                </p>
             </div>
+        </div>
+
+        <div className='flex flex-wrap sm:justify-start justify-center 
+        w-full feedback-container relative z-[1]'>
+            {feedback.map((card) => (
+                <FeedbackCard key={card.id} {...card} />
+            ))}
         </div>
 
     </section>
