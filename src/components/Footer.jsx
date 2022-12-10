06beb7ca1576548e5a1930a15b0a236c07efb9d8
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-10 20:34:38 +0500

    footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import styles from '../style'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className='flex-1 flex flex-col justify-start mr-10'>
+          <img src={logo} alt="hoobank" className='w-[266px] h-[72px] object-contain' />
+          <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the 
+          payments easy, reliable and secure.</p>
+        </div>
+
+        <div className='flex-[1.5] w-full flex flex-row justify-center
+        flex-wrap md:mt-0 mt-10'>
+          {footerLinks.map((footerLink) => (
+            <div key={footerLink.key} className='flex flex-col ss:my-0
+             my-4 min-w-[150px]'>
+
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
